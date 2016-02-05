be6721e0c6b201a95cba97a43d8fe293016db021
Rakavy Ravisingam <rakavy_12@hotmail.com>
2016-02-04 21:19:17 -0500

    Moma Android version; added mapbox sdk

---
+++ Components/mapboxsdk-3.2.0.0/samples/MapboxSampleiOS/MapboxSampleiOS/ViewController.cs
@@ -0,0 +1,81 @@
+﻿using System;
+
+using UIKit;
+using Mapbox;
+using System.Linq;
+using CoreLocation;
+using Foundation;
+
+namespace MapBoxSampleiOS
+{
+    public partial class ViewController : UIViewController, IMapViewDelegate
+    {
+        public ViewController (IntPtr handle) : base (handle)
+        {
+        }
+
+        MapView mapView;
+
+        public override void ViewDidLoad ()
+        {
+            base.ViewDidLoad ();
+
+            // Create a MapView and set the coordinates/zoom
+            mapView = new MapView (View.Bounds);
+            mapView.SetCenterCoordinate (new CLLocationCoordinate2D (45.520486, -122.673541), false);
+            mapView.SetZoomLevel (11, false);
+
+            // Add new annotation
+//            mapView.AddAnnotation (new PointAnnotation {
+//                Coordinate = new CLLocationCoordinate2D (40.7326808, -73.9843407),
+//                Title = "Sample Marker",
+//                Subtitle = "This is the subtitle"
+//            });
+                
+            var coordinates = new [] {
+                new CLLocationCoordinate2D (45.522585, -122.685699),
+                new CLLocationCoordinate2D (45.534611, -122.708873),
+                new CLLocationCoordinate2D (45.530883, -122.678833),
+                new CLLocationCoordinate2D (45.547115, -122.667503),
+                new CLLocationCoordinate2D (45.530643, -122.660121),
+                new CLLocationCoordinate2D (45.533529, -122.636260),
+                new CLLocationCoordinate2D (45.521743, -122.659091),
+                new CLLocationCoordinate2D (45.510677, -122.648792),
+                new CLLocationCoordinate2D (45.515008, -122.664070),
+                new CLLocationCoordinate2D (45.502496, -122.669048),
+                new CLLocationCoordinate2D (45.515369, -122.678489),
+                new CLLocationCoordinate2D (45.506346, -122.702007),
+                new CLLocationCoordinate2D (45.522585, -122.685699),
+            };
+            //nuint numberOfCoordinates = (nuint) (coordinates.Length /  sizeof(CLLocationCoordinate2D));
+
+            // Create our shape with the formatted coordinates array
+            var shape = Polygon.WithCoordinates (coordinates, (nuint)coordinates.Length);
+
+            mapView.AddAnnotation (shape);
+
+            // Set ourselves as the delegate
+            mapView.Delegate = this;
+
+            View.AddSubview (mapView);
+        }
+
+        // Delegate for an annotation to be selected
+        [Export ("mapView:didSelectAnnotation:")]
+        public void DidSelectAnnotation (MapView mapView, Annotation annotation)
+        {
+            // Just show the user which one was pressed
+            new UIAlertView ("Annotation Tapped", "You tapped on: " + annotation.Title, null, "OK")
+                .Show ();
+        }
+
+        public override void DidReceiveMemoryWarning ()
+        {
+            base.DidReceiveMemoryWarning ();
+
+            // Cleanup anything possible
+            mapView.EmptyMemoryCache ();
+        }
+    }
+}
+
---
+++ MomaAndroid/MapViewer.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using Android.App;
+using Mapbox.Views;
+using Mapbox.Annotations;
+using Mapbox.Geometry;
+
+
+[assembly: UsesPermission(Android.Manifest.Permission.AccessNetworkState)]
+[assembly: UsesPermission(Android.Manifest.Permission.Internet)]
+[assembly: UsesPermission(Android.Manifest.Permission.WriteExternalStorage)]
+
+[assembly: UsesPermission(Android.Manifest.Permission.AccessCoarseLocation)]
+[assembly: UsesPermission(Android.Manifest.Permission.AccessFineLocation)]
+[assembly: UsesPermission(Android.Manifest.Permission.AccessWifiState)]
+
+
+namespace MomaAndroid
+{
+    [Activity(Label = "MomaAndroid", MainLauncher = true)]
+    public class MapViewer : Activity
+    {
+        MapView mapView;
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            SetContentView(Resource.Layout.MapView);
+
+            mapView = FindViewById<MapView>(Resource.Id.mapview);
+            mapView.StyleUrl = Mapbox.Constants.Style.Emerald;
+            mapView.CenterCoordinate = new LatLng(41.885, -87.679);
+            mapView.ZoomLevel = 11;
+
+            mapView.OnCreate(bundle);
+
+            mapView.AddMarker(new MarkerOptions()
+                .SetTitle("Test Marker")
+                .SetPosition(new LatLng(41.885, -87.679)));
+        }
+
+        protected override void OnStart()
+        {
+            base.OnStart();
+            mapView.OnStart();
+        }
+
+        protected override void OnStop()
+        {
+            base.OnStop();
+            mapView.OnStop();
+        }
+
+        protected override void OnPause()
+        {
+            base.OnPause();
+            mapView.OnPause();
+        }
+
+        protected override void OnResume()
+        {
+            base.OnResume();
+            mapView.OnResume();
+        }
+
+        protected override void OnDestroy()
+        {
+            base.OnDestroy();
+            mapView.OnDestroy();
+        }
+
+        protected override void OnSaveInstanceState(Bundle outState)
+        {
+            base.OnSaveInstanceState(outState);
+            mapView.OnSaveInstanceState(outState);
+        }
+    }
+}
